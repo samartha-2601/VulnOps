d87c2036fe9165667c3368b407636a9aad8e8b87
Samartha Suresh <samartha.suresh@tamu.edu>
2026-06-07 21:09:21 -0500

    FastAPI setup

---
+++ backend/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="VulnOps API",
+    version="0.1.0"
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to VulnOps"
+    }
+
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "healthy"
+    }
